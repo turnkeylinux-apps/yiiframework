10f559cefcf9f6afc04d199baadacae1a8f8b2df
Alon Swartz <alon@turnkeylinux.org>
2012-06-13 13:23:41 +0300

    tweaked index.php

--- overlay/usr/local/src/yiiframework.overlay/protected/views/site/index.php
+++ overlay/usr/local/src/yiiframework.overlay/protected/views/site/index.php
@@ -2,15 +2,12 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
 	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
 	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
 </ul>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>You may use the <a href="/index.php?r=gii">Gii</a> - a powerful web based code generater to implement CRUD (Create, Read, Update, Delete) operations.</p>
+
+<p>For more information refer to the <a href="http://www.turnkeylinux.org/yiiframework">TurnKey Yii Framework release notes</a>. For more details on how to further develop this application, please read the <a href="http://www.yiiframework.com/doc/">documentation</a>. Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>, should you have any questions.</p>
